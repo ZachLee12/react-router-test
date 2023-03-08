04a17ef9857dc647556ce8256d88546988ce246d
ZachLee12 <leezhengyang22@gmail.com>
2023-03-08 10:02:35 +0100

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createHashRouter, Link } from 'react-router-dom'
+
+const hashRouter = createHashRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/other-page',
+    element: (
+      <div>
+        <h1>Other page</h1>
+        <h2><Link to='/'>Go back to App</Link></h2>
+      </div>
+    )
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={hashRouter} />
   </React.StrictMode>,
 )
